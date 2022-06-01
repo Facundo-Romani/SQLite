4c9cb877f064391fd736b177f0c008b6d1739992
unknown <facundoromani90@gmail.com>
2022-06-01 11:43:49 -0300

    Primer CRUDSqlite

---
+++ CRUDSqlite/CRUDSqlite/CRUDSqlite/Models/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace CRUDSqlite.Models
+{
+    public class Alumno
+    {
+        [PrimaryKey,AutoIncrement]
+        public int IdAlumno { get; set; }
+
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string ApellidoPaterno { get; set; }
+
+        [MaxLength(50)]
+        public string ApellidoMaterno { get; set; }
+
+        public int Edad { get; set; }
+
+        [MaxLength(100)]
+        public  string  Email { get; set; }
+    }
+}
