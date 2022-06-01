eb82e8b800d35c34991e625291d6a7495d7a5b9d
unknown <facundoromani90@gmail.com>
2022-06-01 16:58:19 -0300

    Primer CRUDSqlite

--- CRUDSqlite/CRUDSqlite/CRUDSqlite/Data/SQLiteHelper.cs
+++ CRUDSqlite/CRUDSqlite/CRUDSqlite/Data/SQLiteHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using SQLite;
 using CRUDSqlite.Models;
+using System.Threading.Tasks;
 
 namespace CRUDSqlite.Data
 {
@@ -14,6 +15,39 @@ namespace CRUDSqlite.Data
         {
             db = new SQLiteAsyncConnection(dbPath);
             db.CreateTableAsync<Alumno>().Wait();
+        }
+
+
+        // Insertar.
+        public Task<int> SaveAlumnoAsync(Alumno alum)
+        {
+            if (alum.IdAlumno != 0)
+            {
+                return db.UpdateAsync(alum);
+            }
+            else
+            {
+                return db.InsertAsync(alum);
+            }
+        }
+
+        /// <summary>
+        /// Recuperar todos los Alumnos.
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Alumno>>  GetAlumnosAsync()
+        {
+            return db.Table<Alumno>().ToListAsync();
+        }
+
+        /// <summary>
+        /// Recupera Alumnos por Id
+        /// </summary>
+        /// <param name="idAlumno">Id del alumno que se requiere</param>
+        /// <returns></returns>
+        public Task<Alumno> GetAlumnoByIdAsync (int idAlumno)
+        {
+            return db.Table<Alumno>().Where(a => a.IdAlumno == idAlumno).FirstOrDefaultAsync();
         }
     }
 }
--- CRUDSqlite/CRUDSqlite/CRUDSqlite/MainPage.xaml.cs
+++ CRUDSqlite/CRUDSqlite/CRUDSqlite/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRUDSqlite.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,140 @@ namespace CRUDSqlite
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        // Click para el insert.
+        private async void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            if (validarDatos())
+            {
+                Alumno alum = new Alumno // Objeto que se almacena en base de datos.
+                {
+                    Nombre = txtNombre.Text,
+                    ApellidoPaterno = txtApellidoPaterno.Text,
+                    ApellidoMaterno = txtApellidoMaterno.Text,
+                    Edad = int.Parse(txtEdad.Text),
+                    Email = txtEmail.Text,
+                };
+                await App.SQLiteDB.SaveAlumnoAsync(alum);
+
+                txtNombre.Text = "";
+                txtApellidoPaterno.Text = "";
+                txtApellidoMaterno.Text = "";
+                txtEdad.Text = "";
+                txtEmail.Text = "";
+
+                await DisplayAlert("Registro", "Se guardo de manera exitosa el alumno", "Ok");
+                llenarDatos();
+
+            }
+            else
+            {
+                await DisplayAlert("Advertinecia", "Ingresar todos los datos", "Ok");
+            }
+        }
+
+
+        public async void llenarDatos()
+        {
+
+            var alumnoList = await App.SQLiteDB.GetAlumnosAsync();
+
+            if (alumnoList != null)
+            {
+                LstAlumnos.ItemsSource = alumnoList;
+            }
+        }
+
+
+        public bool validarDatos()
+        {
+            bool respuesta;
+
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtApellidoPaterno.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtApellidoMaterno.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtEdad.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                respuesta = false;
+            }
+            else
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        private async void LstAlumnos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var obj = (Alumno)e.SelectedItem;
+            btnRegistrar.IsVisible = false;
+            txtIdAlumno.IsVisible = true;
+            btnActualizar.IsVisible = true;
+
+            if (!string.IsNullOrEmpty(obj.IdAlumno.ToString()))
+            {
+                var alumno = await App.SQLiteDB.GetAlumnoByIdAsync(obj.IdAlumno);
+                if (alumno != null)
+                {
+                    txtIdAlumno.Text = alumno.IdAlumno.ToString();
+                    txtNombre.Text = alumno.Nombre;
+                    txtApellidoPaterno.Text = alumno.ApellidoPaterno;
+                    txtApellidoMaterno.Text = alumno.ApellidoMaterno;
+                    txtEdad.Text = alumno.Edad.ToString();
+                    txtEmail.Text = alumno.Email;
+                }
+            }
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtIdAlumno.Text))
+            {
+                Alumno alumno = new Alumno()
+                {
+                    IdAlumno=Convert.ToInt32(txtIdAlumno.Text),
+                    Nombre = txtNombre.Text,
+                    ApellidoPaterno = txtApellidoPaterno.Text,
+                    ApellidoMaterno = txtApellidoMaterno.Text,
+                    Edad= Convert.ToInt32(txtEdad),
+                    Email = txtEmail.Text
+                };
+
+                await App.SQLiteDB.SaveAlumnoAsync(alumno);
+                await DisplayAlert("Registro", "Se Actualizo de manera exitosa el alumno" , "ok");
+
+                txtIdAlumno.Text = "";
+                txtNombre.Text = "";
+                txtApellidoPaterno.Text = "";
+                txtApellidoMaterno.Text = "";
+                txtEdad.Text = "";
+                txtEmail.Text = "";
+                txtIdAlumno.IsVisible = false;
+                btnActualizar.IsVisible = true;
+                btnRegistrar.IsVisible = true;
+                llenarDatos();
+               
+            }
+        }
+
+        private void btnActualizar_Clicked_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
